aab65dce0f83fa5b0caf994cbac219f606d095ed
Andre Z. Sanchez <andrezacsanchez@gmail.com>
2014-07-13 01:58:54 -0400

    merp

---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var uniq = require('lodash.uniq')
+
+var CssSelectorParser = require('css-selector-parser').CssSelectorParser
+var cssSelector = new CssSelectorParser()
+
+cssSelector.registerSelectorPseudos('has');
+cssSelector.registerNestingOperators('>', '+', '~');
+cssSelector.registerAttrEqualityMods('^', '$', '*', '~');
+cssSelector.enableSubstitutes();
+
+module.exports = getCssSelectorClasses
+
+/**
+ * Return all the classes in a CSS selector.
+ *
+ * @param {String} selector A CSS selector
+ * @return {String[]} An array of every class present in the CSS selector
+ */
+function getCssSelectorClasses(selector) {
+  var list = []
+  var ast = cssSelector.parse(selector)
+  console.log(ast)
+  visitRules(ast, function(ruleSet) {
+    if (ruleSet.classNames) {
+      list = list.concat(ruleSet.classNames)
+    }
+  })
+  return uniq(list)
+}
+
+function visitRules(node, fn) {
+  if (node.rule) {
+    visitRules(node.rule, fn)
+  }
+  if (node.selectors) {
+    node.selectors.forEach(function(node) {
+      visitRules(node, fn)
+    })
+  }
+  if (node.type === 'rule') {
+    fn(node)
+  }
+}
