f5505a35768cb446cacd6d3f8b84c8ebd5b810bb
Andre Z. Sanchez <andrezacsanchez@gmail.com>
2014-07-15 16:37:06 -0400

    Add test for nested classes

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var test = require('tape')
 var classes = require('./')
 
 test('css selector classes', function(t) {
-  t.plan(8)
+  t.plan(9)
 
   t.deepEquals(classes('.wat'), ['wat'])
 
@@ -17,6 +17,8 @@ test('css selector classes', function(t) {
   t.notEqual(c.indexOf('yo'), -1, 'should return correct classes')
 
   t.deepEqual(classes('.purple article span.purple div.purple.purple.purple'), ['purple'], 'should not create duplicates')
+
+  t.deepEqual(classes('div:not(.hahaha)'), ['hahaha'], 'should support nested classes')
 
   t.throws(function() {
     classes('5')
